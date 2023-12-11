4453479c8b8df6e2923c41ed3a55fcf362f96e4b
coderomayer <omayerahmad1998@gmail.com>
2023-12-11 19:02:18 +0600

    Header aded

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className="p-8 md:p-24 h-screen bg-gradient-to-br from-slate-900 to-zinc-800 darken">
+      <div className="text-white">
+        <h4 className='text-lime-400 font-mono'>Hi, my name is</h4>
+        <h2 className="text-6xl font-Inter font-bold my-3 ">Omayer Ahmad</h2>
+        <h2 className="text-6xl font-Inter font-bold text-gray-400 ">JavaScript & React Enthusiast</h2>
+        <p className='w-2/4 font-Inter font-light my-3 text-gray-400 '>
+          Aspiring front-end developer with a focus on JavaScript and React.
+          Familiar with Node.js and MongoDB, I'm on a journey to deepen my
+          understanding of JavaScript
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
